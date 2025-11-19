fb8c921bb8acc384c6c17a2f03825aa2c8268e80
emils <emils@gmail.com>
2025-11-19 15:01:17 +0200

    PD

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // your code goes here
+const inputWeight = document.getElementById('svars');
+const inputHeight = document.getElementById('augums');
+const outPut = document.getElementById('out');
+
+addEventListener.kg('click', function(){
+    innerHTML = inputWeight / inputHeight;
+    console.log(innerHTML)
+
+})
