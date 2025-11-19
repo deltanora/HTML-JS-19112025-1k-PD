ce071ebbae0da680835b629d2853e9bb2224cd6c
emils <emils@gmail.com>
2025-11-19 15:10:09 +0200

    PD

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const inputHeight = document.getElementById('augums');
 const outPut = document.getElementById('out');
 
 addEventListener.kg('click', function(){
-    innerHTML = inputWeight / inputHeight;
-    console.log(innerHTML)
+    BMI = inputWeight / inputHeight;
+    console.log(BMI)
 
 })
+
+BMI = 0;
+if (BMI < 18,5){
+    console.log("Nepietiekama ķermeņa masa");
+}else{
+    (BMI = 18,5 – 24,99)
+    console.log("Normāla ķermeņa masa")
+}else{
+    (BMI =  25 – 29,99 )
+    console.log("Lieka ķermeņa masa")
+}else{
+    (BMI > 30)
+    console.log("Aptaukošanās")
+}
